b62138b3035c561cec84eec762e5fddd2e912cbf
Dmitry Averin <avedmigen@gmail.com>
2023-12-23 14:23:13 +0300

    Переносит createOffersSectionTemplateTemplate в утилиты точки

--- src/utils/point.js
+++ src/utils/point.js
@@ -138,6 +138,40 @@ const createDestinationDescriptionTemplate = (
   } `
 );
 
+const createOffersSectionTemplateTemplate = (hasTypeOffers, typeOffers, pointOffersIds) => (
+  `${hasTypeOffers ? `
+      <section class="event__section  event__section--offers">
+        <h3 class="event__section-title  event__section-title--offers">Offers</h3>
+        <div class="event__available-offers">
+
+    ${typeOffers.map(({ id, title, price }) => {
+
+    const offerLastWord = title.split(' ').pop().replace(/-/g, '');
+    const checked = pointOffersIds.includes(id) ? 'checked' : '';
+
+    return `
+        <div class="event__offer-selector">
+          <input
+            class="event__offer-checkbox visually-hidden"
+            id="event-offer-${offerLastWord}-${id}"
+            type="checkbox"
+            name="event-offer-${offerLastWord}"
+            ${checked}
+          >
+          <label class="event__offer-label"
+            for="event-offer-${offerLastWord}-${id}">
+            <span class="event__offer-title">${title}</span>
+            +€&nbsp;
+            <span class="event__offer-price">${price}</span>
+          </label>
+      </div>`;
+  }).join('')}
+        </div>
+    </section>
+  ` : ''
+  } `
+);
+
 
 export {
   humanizePointDateTime,
@@ -161,4 +195,5 @@ export {
   createDestinationListTemplate,
   createDestinationPhotosTemplate,
   createDestinationDescriptionTemplate,
+  createOffersSectionTemplateTemplate,
 };
--- src/view/event-edit-view.js
+++ src/view/event-edit-view.js
@@ -9,6 +9,7 @@ import {
   createDestinationListTemplate,
   createDestinationPhotosTemplate,
   createDestinationDescriptionTemplate,
+  createOffersSectionTemplateTemplate,
 } from '../utils/point.js';
 
 import flatpickr from 'flatpickr';
@@ -48,41 +49,11 @@ const createEventEditViewTemplate = (point, destinations, offers) => {
     destinationPhotosTemplate
   );
 
-  const createOffersSectionTemplateTemplate = () => (
-    `${hasTypeOffers ? `
-        <section class="event__section  event__section--offers">
-          <h3 class="event__section-title  event__section-title--offers">Offers</h3>
-          <div class="event__available-offers">
-
-      ${typeOffers.map(({ id, title, price }) => {
-
-      const offerLastWord = title.split(' ').pop().replace(/-/g, '');
-      const checked = pointOffersIds.includes(id) ? 'checked' : '';
-
-      return `
-          <div class="event__offer-selector">
-            <input
-              class="event__offer-checkbox visually-hidden"
-              id="event-offer-${offerLastWord}-${id}"
-              type="checkbox"
-              name="event-offer-${offerLastWord}"
-              ${checked}
-            >
-            <label class="event__offer-label"
-              for="event-offer-${offerLastWord}-${id}">
-              <span class="event__offer-title">${title}</span>
-              +€&nbsp;
-              <span class="event__offer-price">${price}</span>
-            </label>
-        </div>`;
-    }).join('')}
-          </div>
-      </section>
-    ` : ''
-    } `
+  const offersSectionTemplate = createOffersSectionTemplateTemplate(
+    hasTypeOffers,
+    typeOffers,
+    pointOffersIds,
   );
-
-  const offersSectionTemplate = createOffersSectionTemplateTemplate();
 
   return (
     `<li class="trip-events__item" >
