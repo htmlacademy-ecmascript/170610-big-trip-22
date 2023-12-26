baa505fef073dfc0e295cfb17b2c18fba3d63569
Dmitry Averin <avedmigen@gmail.com>
2023-12-26 18:36:17 +0300

    Удаляет лишнее из api направлений и офферов

--- src/api/destinations-api-service.js
+++ src/api/destinations-api-service.js
@@ -1,23 +1,10 @@
 import ApiService from '../framework/api-service.js';
-import { METHOD, URL } from '../const.js';
+import { URL } from '../const.js';
 
 
 export default class DestinationsApiService extends ApiService {
   get destinations() {
     return this._load({ url: `${URL.DESTINATIONS}` })
       .then(ApiService.parseResponse);
-  }
-
-  async updateDestination(destination) {
-    const response = await this._load({
-      url: `${URL.DESTINATIONS}/${destination.id}`,
-      method: METHOD.PUT,
-      body: JSON.stringify(destination),
-      headers: new Headers({ 'Content-Type': 'application/json' }),
-    });
-
-    const parsedResponse = await ApiService.parseResponse(response);
-
-    return parsedResponse;
   }
 }
--- src/api/offers-api-service.js
+++ src/api/offers-api-service.js
@@ -1,23 +1,10 @@
 import ApiService from '../framework/api-service.js';
-import { METHOD, URL } from '../const.js';
+import { URL } from '../const.js';
 
 
 export default class OffersApiService extends ApiService {
   get offers() {
     return this._load({ url: `${URL.OFFERS}` })
       .then(ApiService.parseResponse);
-  }
-
-  async updateDestination(offer) {
-    const response = await this._load({
-      url: `${URL.OFFERS}/${offer.id}`,
-      method: METHOD.PUT,
-      body: JSON.stringify(offer),
-      headers: new Headers({ 'Content-Type': 'application/json' }),
-    });
-
-    const parsedResponse = await ApiService.parseResponse(response);
-
-    return parsedResponse;
   }
 }
