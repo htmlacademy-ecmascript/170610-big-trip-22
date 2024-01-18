c6634fcc7c4db901c9f02a2de623dacf6f5b78b6
Dmitry Averin <avedmigen@gmail.com>
2024-01-18 20:38:56 +0300

    Добавляет условие повторной инициализации борд презентереа если удалены все точки и не сохраняем форму новой точки

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ import OffersModel from './model/offers-model.js';
 import FilterModel from './model/filter-model.js';
 import PointsApiService from './api/points-api-service.js';
 
-const AUTHORIZATION = 'Basic 1R21Y2xa~xz2~232Dp';
+const AUTHORIZATION = 'Basic 1R21Y2xa~xz2~232D2p';
 const END_POINT = 'https://22.objects.pages.academy/big-trip';
 
 const pageBodyElement = document.querySelector('.page-body');
@@ -71,7 +71,13 @@ newEventButtonComponent.element.disabled = true;
 
 function handleNewEventFormClose() {
   newEventButtonComponent.element.disabled = false;
-  boardPresenter.init();
+
+  const pointsCount = pointsModel.points;
+
+  if (pointsCount.length === 0) {
+    boardPresenter.init();
+  }
+
 }
 
 function handleNewEventButtonClick() {
