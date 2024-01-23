97ff3afbb6a3e4dc6bfe51bcf8df373b82782886
Dmitry Averin <avedmigen@gmail.com>
2024-01-23 15:06:09 +0300

    Блокирует кнопку если ошибка загрузки данных с сервера

--- src/main.js
+++ src/main.js
@@ -9,8 +9,8 @@ import OffersModel from './model/offers-model.js';
 import FilterModel from './model/filter-model.js';
 import PointsApiService from './api/points-api-service.js';
 
-const AUTHORIZATION = 'Basic 1R21Y2xa~232Ds2p';
-const END_POINT = 'https://22.objects.pages.academy/big-trips';
+const AUTHORIZATION = 'Basic 1R21Y2xa~232D2p';
+const END_POINT = 'https://22.objects.pages.academy/big-tripы';
 
 const pageBodyElement = document.querySelector('.page-body');
 
@@ -85,17 +85,32 @@ function handleNewEventButtonClick() {
   newEventButtonComponent.element.disabled = true;
 }
 
-infoPresenter.init();
 
+let isError = false;
+
+infoPresenter.init();
 filterPresenter.init();
 render(newEventButtonComponent, tripMainElement);
 boardPresenter.init();
 
+new Promise(() => {
+  pointsApiService.points
+    .then(() => {
+      pointsModel.init()
+        .finally(() => {
+          newEventButtonComponent.element.disabled = isError;
+          render(newEventButtonComponent, tripMainElement);
+        });
+    })
+    .catch(() => {
+      isError = true;
+      pointsModel.init()
+        .finally(() => {
+          newEventButtonComponent.element.disabled = isError;
+          render(newEventButtonComponent, tripMainElement);
+        });
 
-pointsModel.init()
-  .finally(() => {
-    newEventButtonComponent.element.disabled = false;
-    render(newEventButtonComponent, tripMainElement);
-  });
+    });
+});
 
 
